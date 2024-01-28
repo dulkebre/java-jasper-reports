d0527066a111c7093f4aabdcffd5b403af74af39
dulkebre <dulkebre@gmail.com>
2024-01-28 10:24:06 +0100

    First presentable report solution

---
+++ src/main/java/bre/dulke/jasperreports/Main.java
@@ -0,0 +1,80 @@
+package bre.dulke.jasperreports;
+
+import static bre.dulke.jasperreports.SimpleReportFiller.REPORT_ROOT;
+
+import java.io.IOException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.sf.jasperreports.engine.data.JRMapCollectionDataSource;
+
+public class Main {
+
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) throws IOException {
+		logger.info("Lets get busy!");
+		SimpleReportFiller simpleReportFiller = new SimpleReportFiller();
+
+		simpleReportFiller.setReportFileName("forestsDigitalReport.jrxml");
+		simpleReportFiller.compileReport();
+
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("title", "Sample Jasper Reports");
+		parameters.put("companyName", "Forests Digital");
+		parameters.put("clientName", "My first client");
+
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		URL url = classloader.getResource("logo.jpg");
+
+		parameters.put("logo", url.getPath());
+
+		List<Map<String, ?>> reportData = new ArrayList<Map<String, ?>>();
+		reportData.add(createNewRowData(1, "First item description", 3.0, createDate("2023-12-28"), 17500));
+		reportData.add(createNewRowData(2, "Second item description", 4.0, createDate("2023-01-29"), 0.33));
+		reportData.add(createNewRowData(2, "Third item description", 42.0, null, 131));
+
+		JRMapCollectionDataSource hardcodedDataSource = new JRMapCollectionDataSource(reportData);
+		simpleReportFiller.setParameters(parameters);
+		simpleReportFiller.setDataSource(hardcodedDataSource);
+		simpleReportFiller.fillReport();
+
+		SimpleReportExporter simpleExporter = new SimpleReportExporter();
+		simpleExporter.setJasperPrint(simpleReportFiller.getJasperPrint());
+
+		simpleExporter.exportToPdf(REPORT_ROOT + "forestsDigitalReport.pdf", "Author - Nikola");
+		simpleExporter.exportToXlsx(REPORT_ROOT + "forestsDigitalReport.xlsx", "Sheet - Test");
+		simpleExporter.exportToCsv(REPORT_ROOT + "forestsDigitalReport.csv");
+		simpleExporter.exportToHtml(REPORT_ROOT + "forestsDigitalReport.html");
+
+		logger.info("Voila - reports have been created");
+
+	}
+
+	private static Map<String, Object> createNewRowData(int orderItem, String description, double quantity, Date date, double price) {
+		Map<String, Object> result = new HashMap<>();
+		result.put("order-item", Integer.valueOf(orderItem));
+		result.put("order-item-description", description);
+		result.put("order-item-quantity", Double.valueOf(quantity));
+		result.put("order-date", date);
+		result.put("order-item-total-price", Double.valueOf(price));
+		return result;
+	}
+	
+	public static Date createDate(String date) {
+	     try {
+	         return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+	     } catch (ParseException e) {
+	         return null;
+	     }
+	  }
+}
---
+++ src/main/java/bre/dulke/jasperreports/SimpleReportFiller.java
@@ -0,0 +1,86 @@
+package bre.dulke.jasperreports;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRSaver;
+
+public class SimpleReportFiller {
+	
+	public static final String REPORT_ROOT = "reports/";
+	
+    private String reportFileName;
+
+    private JasperReport jasperReport;
+
+    private JasperPrint jasperPrint;
+
+    private JRDataSource dataSource;
+
+    private Map<String, Object> parameters;
+
+    public SimpleReportFiller() {
+        parameters = new HashMap<>();
+    }
+
+    public void prepareReport() {
+        compileReport();
+        fillReport();
+    }
+
+    public void compileReport() {
+        try {
+            InputStream reportStream = getClass().getResourceAsStream("/".concat(reportFileName));
+            jasperReport = JasperCompileManager.compileReport(reportStream);
+            JRSaver.saveObject(jasperReport, REPORT_ROOT + reportFileName.replace(".jrxml", ".jasper"));
+        } catch (JRException ex) {
+            Logger.getLogger(SimpleReportFiller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void fillReport() {
+        try {
+            jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        } catch (JRException ex) {
+            Logger.getLogger(SimpleReportFiller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public JRDataSource getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(JRDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Map<String, Object> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Map<String, Object> parameters) {
+        this.parameters = parameters;
+    }
+
+    public String getReportFileName() {
+        return reportFileName;
+    }
+
+    public void setReportFileName(String reportFileName) {
+        this.reportFileName = reportFileName;
+    }
+
+    public JasperPrint getJasperPrint() {
+        return jasperPrint;
+    }
+
+}
